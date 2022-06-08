0d8e26792785a91a197c174d18775bed4b5c57c3
Alessandre Martins <sandremartins@gmail.com>
2022-06-08 16:17:39 -0400

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Alessandre Martins on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
